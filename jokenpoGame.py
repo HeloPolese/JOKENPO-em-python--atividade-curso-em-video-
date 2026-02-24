8f6d8fe55bf40b46540af1aa40b8ce694b6d3b68
Heloisa Polese <poleseheloisa24@gmail.com>
2026-02-24 09:16:45 -0300

    Add files via upload
    código

---
+++ jokenpoGame.py
@@ -0,0 +1,37 @@
+import random
+from time import sleep
+user = int(input("Digite:\n[0] PEDRA\n[1] PAPEL\n[2] TESOURA  "))
+options = ["PEDRA", "PAPEL", "TESOURA"]
+computador = random.choice(options)
+
+print("\nEscolha do computador: {}".format(computador) )
+print("Escolha do user: {}".format(options[user]) )
+
+print("\nJO")
+sleep(1)
+print("KEN")
+sleep(1) #sleep é tipo uma pausa antes da msg
+print("PO!\n")
+
+if user == 0 and computador == "PEDRA":
+        print("EMPATE")
+elif user == 0 and computador == "TESOURA":
+    print("PARÁBENS USER! VOCÊ GANHOU!")
+elif user == 0 and computador == "PAPEL":
+    print("VOCÊ PERDEU!")
+
+elif user == 1 and computador == "PAPEL":
+    print("EMPATE!")
+elif user == 1 and computador == "PEDRA":
+    print("PARÁBENS USER! VOCÊ GANHOU!")
+elif user == 1 and computador ==  "TESOURA":
+    print("VOCÊ PERDEU!")
+
+elif user == 2 and computador == "TESOURA":
+    print("EMPATE!")
+elif user == 2 and computador ==  "PAPEL":
+    print("PARÁBENS USER! VOCÊ GANHOU!")
+elif user == 2 and computador == "PEDRA":
+    print("VOCÊ PERDEU!")
+else:
+    print("DIGITE UM NÚMERO DE 0 A 2!")
